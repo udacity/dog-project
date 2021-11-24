bcc1fedc9630968be36b619a458f32cb4b803157
NGKOKTHAI <ngkokthai100@gmail.com>
2021-11-24 09:09:17 +0800

    dog

--- extract_bottleneck_features.py
+++ extract_bottleneck_features.py
@@ -7,7 +7,7 @@ def extract_VGG19(tensor):
 	return VGG19(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
 
 def extract_Resnet50(tensor):
-	from keras.applications.resnet50 import ResNet50, preprocess_input
+	from keras.applications.resnet import ResNet50, preprocess_input
 	return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
 
 def extract_Xception(tensor):
