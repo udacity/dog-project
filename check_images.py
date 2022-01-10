530b7b79d19688a76d01e8adf4f4513d165a3caf
Han Sheng <hanshengliang@outlook.com>
2022-01-10 14:00:14 +0800

    :zap: Establish New Training Pipeline

--- check_images.py
+++ check_images.py
@@ -5,56 +5,17 @@ import glob
 
 fileList = glob.glob('./dogImages/*/*/*.*')
 
-marker_mapping = {
-    0xffd8: "Start of Image",
-    0xffe0: "Application Default Header",
-    0xffdb: "Quantization Table",
-    0xffc0: "Start of Frame",
-    0xffc4: "Define Huffman Table",
-    0xffda: "Start of Scan",
-    0xffd9: "End of Image"
-}
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
+for i, image_name in enumerate(fileList):
+    print(i, image_name)
 
-class JPEG:
-    def __init__(self, image_file):
-        with open(image_file, 'rb') as f:
-            self.img_data = f.read()
-
-    def decode(self):
-        data = self.img_data
-        while (True):
-            marker, = unpack(">H", data[0:2])
-            # print(marker_mapping.get(marker))
-            if marker == 0xffd8:
-                data = data[2:]
-            elif marker == 0xffd9:
-                return
-            elif marker == 0xffda:
-                data = data[-2:]
-            else:
-                lenchunk, = unpack(">H", data[2:4])
-                data = data[2 + lenchunk:]
-            if len(data) == 0:
-                break
-
-
-bads = []
-
-root_dir = ""
-
-for img in fileList:
-
-    image = img
-    image = JPEG(image)
-    try:
-        image.decode()
-    except:
-        bads.append(img)
-
-for name in bads:
-    print(img)
-    try:
-        os.remove(img)
-    except:
-        pass
+    with tf.Graph().as_default():
+        image_contents = tf.read_file(image_name)
+        image = tf.image.decode_jpeg(image_contents, channels=3)
+        #
+        with tf.Session() as sess:
+            #sess.run(init_op)
+            sess.run(tf.global_variables_initializer())
+            tmp = sess.run(image)
